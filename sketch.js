7ef8ae0c16fa211750f8a8acb69286423a3b8f23
byjusmx2 <87038490+byjusmx2@users.noreply.github.com>
2021-09-30 12:16:19 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -65,7 +65,7 @@ function setup() {
   obstaclesGroup = createGroup();
   cloudsGroup = createGroup();
   
-  console.log("Hello" + 5);
+  console.log("Hola" + 5);
   
   trex.setCollider("circle",0,0,40);
   trex.debug = true
